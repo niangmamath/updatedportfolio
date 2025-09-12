253da4a62d9ffe35d25c54cc674e15ea8eccf5d6
niangmamath <niangmamathyacine@gmail.com>
2025-09-13 00:30:55 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -245,10 +245,8 @@ function App() {
         </div>
         <div className="container mx-auto px-4 md:px-6">
           <div className="text-center mb-16">
-            <h2 className="text-3xl md:text-5xl font-bold mb-4 bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
-              Projets Innovants
-            </h2>  
-             <h2 className="text-3xl md:text-5xl font-bold mb-4 bg-gradient-to-r from-pink-400 to-red-400 bg-clip-text text-transparent">
+            
+            <h2 className="text-3xl md:text-5xl font-bold mb-4 bg-gradient-to-r from-pink-400 to-red-400 bg-clip-text text-transparent">
              Projets innovants
             </h2>
             <p className="text-xl text-white max-w-2xl mx-auto">
